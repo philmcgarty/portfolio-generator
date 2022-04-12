2b95bce351b34f415c7ec59903a32e0dbed4ae0b
Phil McGarty <philmcgarty@hotmail.com>
2022-04-11 21:24:54 -0400

    generate html

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = (profileDataArr) => {
-    
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    
-};
+const [name, github] = profileDataArgs;
 
 
 
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err);
 
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
